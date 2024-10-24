a4dd862e581a165619cd82af7a508d694401978b
mikeynp <mikeynp@mac.home>
2024-10-24 17:16:12 +0100

    Fix warning about async_forward_entry_setup

--- custom_components/mixergy/__init__.py
+++ custom_components/mixergy/__init__.py
@@ -43,7 +43,7 @@ async def async_setup_entry(hass: HomeAssistant, entry:ConfigEntry) -> bool:
     _register_services(hass)
 
     for component in PLATFORMS:
-        hass.async_create_task(hass.config_entries.async_forward_entry_setup(entry, component))
+        await hass.config_entries.async_forward_entry_setup(entry, component)
 
     return True
 
